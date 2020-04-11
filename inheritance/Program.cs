c2050a3beffb3324da8a2047159b6523ae04b317
arybayunurwicaksono <ary.bayunurw45@gmail.com>
2020-04-11 11:16:16 +0700

    Tugas tugas

---
+++ inheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Arbync", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Bambang", 36, "123123", "Physical Distancing");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Ary Ganteng", 16, "19.11.2762", "ary.bayunurw45@gmail.com");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
